5de8143a58a3572c06e95a158b7bef058020383c
jack_kerouac <jack_kerouac>
2008-12-19 15:25:24 +0000

    - added vCard example - added mindmaps and visio files

--- 07_Wissensspeicher/Anwendungsfalldiagramm.mm
+++ 07_Wissensspeicher/Anwendungsfalldiagramm.mm
@@ -1,11 +1,23 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1229008309012" ID="Freemind_Link_568784843" MODIFIED="1229008316050" TEXT="Anwendungsfalldiagramm">
-<node CREATED="1229008337067" ID="_" MODIFIED="1229008338675" POSITION="right" TEXT="Akteur">
+<node CREATED="1229008309012" ID="Freemind_Link_568784843" MODIFIED="1229502955602" TEXT="Anwendungsfall">
+<node CREATED="1229503198177" ID="Freemind_Link_1102119429" MODIFIED="1229503199461" POSITION="right" TEXT="purpose">
+<node CREATED="1229503200601" ID="Freemind_Link_457485527" MODIFIED="1229503208853" TEXT="capture functional requirements"/>
+<node CREATED="1229504353643" ID="Freemind_Link_1178689094" MODIFIED="1229505193595" TEXT="&quot;Each use case describes how the Actor will interact with the system to achieve a specific goal&quot;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1229504461345" ID="Freemind_Link_146360908" MODIFIED="1229504468253" POSITION="right" TEXT="one or more scenarios per use case">
+<node CREATED="1229504472521" ID="Freemind_Link_34194533" MODIFIED="1229504486101" TEXT="one fore ach possible way to achieve the use cases&apos; goal"/>
+</node>
+<node CREATED="1229502944077" ID="Freemind_Link_777232939" MODIFIED="1229502949956" POSITION="right" TEXT="UML-Anwendungsfalldiagramm">
+<node CREATED="1229504436450" ID="Freemind_Link_1536639808" MODIFIED="1229504445373" TEXT="overview of system use cases"/>
+<node CREATED="1229008337067" ID="_" MODIFIED="1229008338675" TEXT="Akteur">
 <node CREATED="1229008339304" ID="Freemind_Link_484565336" MODIFIED="1229009024798" TEXT="wer interagiert mit System">
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1229008391806" ID="Freemind_Link_573736545" MODIFIED="1229008396578" TEXT="wer benutzt das System"/>
 <node CREATED="1229008397167" ID="Freemind_Link_1373224929" MODIFIED="1229008419666" TEXT="wer wird vom System benutzt (Bereitstellung von Funktionalit&#xe4;t)"/>
+<node CREATED="1229504965112" ID="Freemind_Link_1937751183" MODIFIED="1229504968532" TEXT="Rolle einer Person!"/>
 </node>
 <node CREATED="1229008424998" ID="Freemind_Link_774304422" MODIFIED="1229008430226" TEXT="Typen, keine Objekte!"/>
 <node CREATED="1229008537059" ID="Freemind_Link_1830435965" MODIFIED="1229008540888" TEXT="Symbol">
@@ -14,7 +26,7 @@
 </node>
 <node CREATED="1229010738972" ID="Freemind_Link_1586060799" MODIFIED="1229010746416" TEXT="Beziehungen zw. Akteuren"/>
 </node>
-<node CREATED="1229008555324" ID="Freemind_Link_1625009846" MODIFIED="1229008560279" POSITION="right" TEXT="Anwendungsfall">
+<node CREATED="1229008555324" ID="Freemind_Link_1625009846" MODIFIED="1229008560279" TEXT="Anwendungsfall">
 <node CREATED="1229008570619" ID="Freemind_Link_39713383" MODIFIED="1229008575855" TEXT="Verhalten, das vom System erwartet wird"/>
 <node CREATED="1229009001891" ID="Freemind_Link_1459196534" MODIFIED="1229009009908" TEXT="Beziehungen zw. Anwendungsf&#xe4;llen">
 <node CREATED="1229009014355" ID="Freemind_Link_770688489" MODIFIED="1229009027590" TEXT="include">
@@ -39,11 +51,88 @@
 </node>
 </node>
 </node>
-<node CREATED="1229008985084" ID="Freemind_Link_1198318686" MODIFIED="1229009310248" POSITION="left" TEXT="(Sub-)Systemgrenze">
+<node CREATED="1229008985084" ID="Freemind_Link_1198318686" MODIFIED="1229009310248" TEXT="(Sub-)Systemgrenze">
 <font NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1229093486269" ID="Freemind_Link_1756632744" MODIFIED="1229093492519" POSITION="left" TEXT="Beziehungen zw. Akteur und Anwendungsfall">
+<node CREATED="1229093486269" ID="Freemind_Link_1756632744" MODIFIED="1229093492519" TEXT="Beziehungen zw. Akteur und Anwendungsfall">
 <node CREATED="1229342836384" ID="Freemind_Link_418227265" MODIFIED="1229342848338" TEXT="Akteur nutzt Systemverhalten, das von Use Case beschrieben wird"/>
+</node>
+</node>
+<node CREATED="1229505019911" ID="Freemind_Link_1787042023" MODIFIED="1229505038714" POSITION="left" TEXT="abstraction level">
+<node CREATED="1229505027287" ID="Freemind_Link_956908198" MODIFIED="1229505031202" TEXT="business use case">
+<node CREATED="1229505098398" ID="Freemind_Link_901511138" MODIFIED="1229505102089" TEXT="technology free language"/>
+<node CREATED="1229505102501" ID="Freemind_Link_1617857436" MODIFIED="1229505105265" TEXT="system as black box"/>
+</node>
+<node CREATED="1229505106381" ID="Freemind_Link_1076779993" MODIFIED="1229505108481" TEXT="system use case">
+<node CREATED="1229505124181" ID="Freemind_Link_1116176034" MODIFIED="1229505130257" TEXT="function or the service the system provides"/>
+<node CREATED="1229505144741" ID="Freemind_Link_157575434" MODIFIED="1229505149008" TEXT="often begins with verb">
+<node CREATED="1229505149908" ID="Freemind_Link_909426857" MODIFIED="1229505154176" TEXT="e.g. create order"/>
+</node>
+</node>
+</node>
+<node CREATED="1229505212323" ID="Freemind_Link_872212087" MODIFIED="1229505215871" POSITION="left" TEXT="levels of detail">
+<node CREATED="1229505216507" ID="Freemind_Link_1725089848" MODIFIED="1229505226383" TEXT="brief use case">
+<node CREATED="1229505240939" ID="Freemind_Link_529241136" MODIFIED="1229505247222" TEXT="a few sentences description"/>
+</node>
+<node CREATED="1229505248875" ID="Freemind_Link_1862335975" MODIFIED="1229505253678" TEXT="casual use case">
+<node CREATED="1229505257243" ID="Freemind_Link_1256350754" MODIFIED="1229505262166" TEXT="a few paragraphs of text"/>
+</node>
+<node CREATED="1229505264490" ID="Freemind_Link_1698865590" MODIFIED="1229505270646" TEXT="fully dressed use case">
+<node CREATED="1229505274546" ID="Freemind_Link_579351958" MODIFIED="1229505288558" TEXT="formal document based on template"/>
+</node>
+</node>
+<node CREATED="1229505438751" ID="Freemind_Link_1594431961" MODIFIED="1229505441163" POSITION="left" TEXT="use case templates">
+<node CREATED="1229505722266" ID="Freemind_Link_975394706" MODIFIED="1229505723941" TEXT="main body">
+<node COLOR="#0033ff" CREATED="1229505511710" ID="Freemind_Link_553550060" MODIFIED="1229506449314" TEXT="use case name">
+<node CREATED="1229505521374" ID="Freemind_Link_390258602" MODIFIED="1229505524361" TEXT="ver-noun form"/>
+<node CREATED="1229505562973" ID="Freemind_Link_915166657" MODIFIED="1229505566480" TEXT="no more than four words"/>
+</node>
+<node CREATED="1229505585148" ID="Freemind_Link_851946892" MODIFIED="1229505586736" TEXT="version">
+<node CREATED="1229505629723" ID="Freemind_Link_154935628" MODIFIED="1229505635631" TEXT="use cases can evolve"/>
+<node CREATED="1229505636267" ID="Freemind_Link_1912858640" MODIFIED="1229505641279" TEXT="former, it was a business use case"/>
+<node CREATED="1229505642403" ID="Freemind_Link_975964364" MODIFIED="1229505645847" TEXT="now, more detailed"/>
+<node CREATED="1229505646299" ID="Freemind_Link_1789440542" MODIFIED="1229505649695" TEXT="=&gt; older versions matter!"/>
+</node>
+<node COLOR="#0033ff" CREATED="1229505651003" ID="Freemind_Link_395873592" MODIFIED="1229506458372" TEXT="goal">
+<node CREATED="1229505664707" ID="Freemind_Link_105098332" MODIFIED="1229505671230" TEXT="what the user tries to achieve with this use case"/>
+</node>
+<node COLOR="#0033ff" CREATED="1229505673619" ID="Freemind_Link_674096228" MODIFIED="1229506460828" TEXT="summary">
+<node CREATED="1229505705458" ID="Freemind_Link_1934279130" MODIFIED="1229505710918" TEXT="a few sentences"/>
+<node CREATED="1229505712810" ID="Freemind_Link_442282643" MODIFIED="1229505717838" TEXT="includes goal and primary actor"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1229505735818" ID="Freemind_Link_1160452762" MODIFIED="1229506469108" TEXT="actors">
+<node CREATED="1229505755105" ID="Freemind_Link_674753436" MODIFIED="1229505757693" TEXT="primary actor">
+<node CREATED="1229505758097" ID="Freemind_Link_1461421777" MODIFIED="1229505767221" TEXT="someone/something who acts on the system"/>
+</node>
+<node CREATED="1229505768609" ID="Freemind_Link_1193025199" MODIFIED="1229505777964" TEXT="secondary actor">
+<node CREATED="1229505778369" ID="Freemind_Link_1738842884" MODIFIED="1229505785252" TEXT="someone who is acted on by the system"/>
+</node>
+</node>
+<node CREATED="1229506119482" ID="Freemind_Link_287184572" MODIFIED="1229506124806" TEXT="activation">
+<node CREATED="1229505875959" ID="Freemind_Link_527542086" MODIFIED="1229505877667" TEXT="preconditions">
+<node CREATED="1229505891303" ID="Freemind_Link_571487776" MODIFIED="1229505894346" TEXT="state of the system"/>
+<node CREATED="1229505894758" ID="Freemind_Link_809349099" MODIFIED="1229505912874" TEXT="for trigger to initiate use case"/>
+</node>
+<node CREATED="1229505983973" ID="Freemind_Link_968438275" MODIFIED="1229505986609" TEXT="triggers">
+<node CREATED="1229505986941" ID="Freemind_Link_20529450" MODIFIED="1229505996632" TEXT="the events, that cause the use case to initiate"/>
+<node CREATED="1229506003365" ID="Freemind_Link_1029984372" MODIFIED="1229506008872" TEXT="external, temporal or internal"/>
+</node>
+<node CREATED="1229506286503" ID="Freemind_Link_1952874350" MODIFIED="1229506288923" TEXT="postconditions">
+<node CREATED="1229506290855" ID="Freemind_Link_693758944" MODIFIED="1229506312242" TEXT="state of the system after use case"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1229506111634" ID="Freemind_Link_1513524099" MODIFIED="1229506473188" TEXT="series of events">
+<node CREATED="1229506033532" ID="Freemind_Link_280910834" MODIFIED="1229506036200" TEXT="basic course of events">
+<node CREATED="1229506045268" ID="Freemind_Link_1987057121" MODIFIED="1229506048647" TEXT="primary scenario"/>
+<node CREATED="1229506048892" ID="Freemind_Link_1133337122" MODIFIED="1229506060911" TEXT="&quot;happy path&quot;"/>
+<node CREATED="1229506066987" ID="Freemind_Link_457669936" MODIFIED="1229506069295" TEXT="numbered steps"/>
+</node>
+<node CREATED="1229506101595" ID="Freemind_Link_406147405" MODIFIED="1229506103830" TEXT="alternative paths"/>
+</node>
+<node CREATED="1229506327238" ID="Freemind_Link_597386442" MODIFIED="1229506329650" TEXT="business rules"/>
+<node COLOR="#0033ff" CREATED="1229506329894" ID="Freemind_Link_396349329" MODIFIED="1229506478084" TEXT="notes"/>
+<node CREATED="1229506343646" ID="Freemind_Link_320992491" MODIFIED="1229506346418" TEXT="author and date"/>
 </node>
 </node>
 </map>
